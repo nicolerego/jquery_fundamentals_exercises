9d1c4a89937a27ddc6c18d5d23bafeed191f84b9
Nicole Rego <nicole.rego@live.com>
2015-02-19 20:15:47 -0500

    jQuery exercises completed

---
+++ jquery_fundamentals/jquery_exercises.js
@@ -0,0 +1,88 @@
+// SOLUTIONS GO BELOW EACH EXERCISE
+
+// EXERCISE: Change Background
+// $(document).ready(function() { 
+// 	$('#target').css('background-color', 'blue');
+// });
+
+// EXERCISE: Change Parent
+// $(document).ready(function() { 
+// 	$('#target span').text("New text!");
+// });
+
+// EXERCISE: Create Clone
+// $(document).ready(function() { 
+// 	$('#target span').clone().insertAfter("span");
+// });
+
+// EXERCISE: Use Filter
+// $(document).ready(function() { 
+// 	$('.target').eq(1).css('background-color', 'yellow');
+// });
+
+// EXERCISE: Disable Buttons
+// $(document).ready(function() { 
+// 	$('button').attr('disabled', true);
+// });
+
+// EXERCISE: Uncheck CheckBoxes
+// $(document).ready(function() { 
+// 	$('input').removeAttr('checked');
+// });
+
+// EXERCISE: Change Parent
+// $(document).ready(function() { 
+// 	$('#child').appendTo('#parent2');
+// });
+
+// EXERCISE: Select A Option in A Select Box
+// $(document).ready(function() { 
+// 	$('option').eq(1).attr('selected', true);	
+// });
+
+// EXERCISE: Change Size
+// $(document).ready(function() { 
+// 	$('#target').width("300px");
+// 	$('#target').height("200px");
+// });
+
+// EXERCISE: Empty Elements
+// $(document).ready(function() { 
+// 	$('#target').empty();
+// });
+
+// EXERCISE: Delay
+// $(document).ready(function() { 
+// 	alert("Hello there!").setTimeout(1000);
+// });
+
+// EXERCISE: Count
+// $(document).ready(function() { 
+// 	alert($('#target').find('.child').length);
+// });
+
+// EXERCISE: Animate
+// $(document).ready(function() { 	
+// 	$( "#target" ).click(function() {
+// 	  $( "#target" ).animate({
+// 	    width: "300px",
+// 	    height:"380px",
+// 	  }, 1000 );
+// 	});
+// });
+
+// EXERCISE: Alternate Color
+// $(document).ready(function() { 
+// 	$('option').filter( ":odd" ).css("color", "red");
+// });
+
+// EXERCISE: All But One
+// $(document).ready(function() { 
+// 	$('#target').children().not('h2').remove();
+// });
+
+// EXERCISE: Without Children
+// $(document).ready(function() { 
+// 	$('.target').has('span').addClass("dontDelete")
+// 	$('.target').not('.dontDelete').remove();
+// });
